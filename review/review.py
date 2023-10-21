96475c149870fd5f174f760cc3838c03c4115ca8
Nick Zentmayer <nick_zentmayer@outlook.com>
2023-10-21 15:42:08 -0400

    Add review files

--- review/review.py
+++ review/review.py
@@ -1,1 +1,6 @@
 #Make UI for reviewing flash cards
+from taipy import Gui
+
+# A dark mode is available in Taipy
+# However, we will use the light mode for the Getting Started
+Gui(page="# Getting started with *Taipy*").run(dark_mode=True)
