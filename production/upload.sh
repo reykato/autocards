bc8feac83cad51ab61099fb09f2df595e928f204
Mark Peschel <mpeschel10@gmail.com>
2023-10-22 13:01:02 -0400

    Don't upload .gitignore

--- production/upload.sh
+++ production/upload.sh
@@ -1,2 +1,2 @@
-rsync -av --exclude /.git/ --exclude /README.md --exclude /.vscode --exclude /production/upload.sh --exclude __pycache__ --delete /home/mpeschel/projects/autocards/  autocards:/opt/sites/autocards
+rsync -av --exclude /.git/ --exclude .gitignore --exclude /README.md --exclude /.vscode --exclude /production/upload.sh --exclude __pycache__ --delete /home/mpeschel/projects/autocards/  autocards:/opt/sites/autocards
 ssh -t autocards "sudo nginx -t && sudo nginx -s reload; sudo systemctl daemon-reload && sudo systemctl restart autocards && echo reloaded server ok"
