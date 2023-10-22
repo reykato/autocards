f4dc6c834c139d947230da110417874228a6ec72
Tyler Lindsay <tyty464646@gmail.com>
2023-10-22 01:03:36 -0400

    commit before leaving sat night

--- autocards_app/urls.py
+++ autocards_app/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('browse_cards/', views.get_cards, name="browse_cards"),
+    path('browse_cards/<str:deck_name>/', views.get_cards, name='browse_cards'),
     path('browse_decks/', views.get_decks, name="browse_decks"),
+    path('edit_deck/<str:deck_name>/', views.edit_deck, name='edit_deck'),
+    path('add_deck/', views.add_deck, name='add_deck'),
     path('add_card/', views.add_card, name="add_card"),
     path('sortbynextdue/', views.sort_by_next_due, name="sortbyid"),
-    path('sortbyspacing/', views.sort_by_spacing, name="sortbyname")
+    path('sortbyspacing/', views.sort_by_spacing, name="sortbyname"),
 ]
